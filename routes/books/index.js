15f7821569ecf2007999ee61052d2d16773a07ce
Ernest Misha <pmiel13@gmail.com>
2023-09-04 22:03:48 +0200

    feat: add get book endpoint

--- routes/books/index.js
+++ routes/books/index.js
@@ -1,29 +1,30 @@
 import { Books } from "../../database/models/books.js";
+import { listBooksSchema, getBookSchema } from "../../schemas/books.js";
 
 export default async function (fastify, opts) {
   fastify.get(
     "/",
     {
-      schema: {
-        description: "returns list of books isbn",
-        tags: ["books"],
-        additionalProperties: false,
-        response: {
-          200: {
-            description: "Successful response",
-            type: "array",
-            items: {
-              type: "object",
-              properties: {
-                isbn: { type: "string" },
-              },
-            },
-          },
-        },
-      },
+      schema: listBooksSchema,
     },
     async function (request, reply) {
       return Books.listBooks();
+    }
+  );
+
+  fastify.get(
+    "/:isbn",
+    {
+      schema: getBookSchema,
+    },
+    async function (request, reply) {
+      const book = await Books.getBook(request.params.isbn);
+
+      if (!book) {
+        return reply.notFound();
+      }
+
+      return book;
     }
   );
 }
