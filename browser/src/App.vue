c5bec85853b121a2df1b9edc5b4c1964fe206f4a
Przemysław Mielniczek <pmiel13@gmail.com>
2024-01-28 19:21:56 +0100

    feat(browser): init browser part

---
+++ browser/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <section class="flex min-h-screen flex-col items-center justify-center space-y-8 bg-black">
+    <h1 class="text-4xl text-amber-400">Coming soon...</h1>
+    <button
+      class="rounded-lg border border-rose-300 bg-red-400 p-2 hover:bg-orange-400 active:bg-yellow-400"
+      @click="count++"
+    >
+      Counter: {{ count }}
+    </button>
+  </section>
+</template>
