9ab38563655ae933182c0d5db53fec4fe5a3daf0
Przemysław Mielniczek <pmiel13@gmail.com>
2024-02-09 13:26:12 +0100

    ref(server): refactor app architecture

--- server/src/routes/books/index.ts
+++ server/src/routes/books/index.ts
@@ -1,48 +1,11 @@
 import { FastifyPluginAsync } from 'fastify';
-import { Book } from '../../database';
-import { booksSchema } from '../../schemas';
-import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 const books: FastifyPluginAsync = async (fastify, opts) => {
-  fastify.withTypeProvider<ZodTypeProvider>().get('/', booksSchema.list, (req, rep) => {
-    return Book.listBooks();
-  });
-
-  fastify
-    .withTypeProvider<ZodTypeProvider>()
-    .get('/:isbn', booksSchema.get, async (request, reply) => {
-      const book = await Book.getBook(Number(request.params.isbn));
-
-      if (!book) {
-        return reply.notFound();
-      }
-
-      return book;
-    });
-
-  fastify
-    .withTypeProvider<ZodTypeProvider>()
-    .post('/', booksSchema.create, async function (request) {
-      await Book.createBook(request.body);
-
-      return { isbn: request.body.isbn };
-    });
-
-  fastify
-    .withTypeProvider<ZodTypeProvider>()
-    .patch('/:isbn', booksSchema.update, async function (request) {
-      const isbn = Number(request.params.isbn);
-
-      await Book.updateBook(isbn, request.body.availableCount);
-
-      return { isbn };
-    });
-
-  fastify
-    .withTypeProvider<ZodTypeProvider>()
-    .delete('/:isbn', booksSchema.delete, async function (request) {
-      await Book.deleteBook(Number(request.params.isbn));
-    });
+  fastify.get('/', fastify.schema.list, fastify.controller.list);
+  fastify.get('/:isbn', fastify.schema.get, fastify.controller.get);
+  fastify.post('/', fastify.schema.create, fastify.controller.create);
+  fastify.patch('/:isbn', fastify.schema.update, fastify.controller.update);
+  fastify.delete('/:isbn', fastify.schema.delete, fastify.controller.delete);
 };
 
 export default books;
--- shared/schemas/index.ts
+++ shared/schemas/index.ts
@@ -1,12 +1,11 @@
 import { z } from 'zod';
 
-const isbn = z.number().int().min(1000000000000).max(9999999999999);
 const availableCount = z.number().int().min(0).max(10000);
 
 export const bookSchema = z
   .object({
     title: z.string().min(5).max(40),
-    isbn,
+    isbn: z.number().int().min(1000000000000).max(9999999999999),
     authors: z.string().array().nonempty(),
     edition: z.number().int().min(1).max(30),
     length: z.number().int().min(5).max(2000),
@@ -16,4 +15,3 @@ export const bookSchema = z
   .refine(({ totalCount, availableCount }) => availableCount <= totalCount);
 
 export const updateBookSchema = z.object({ availableCount });
-export const isbnSchema = z.object({ isbn });
