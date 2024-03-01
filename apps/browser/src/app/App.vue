98b3403967ac8466f72e344aadd3fda233a0208c
Przemysław Mielniczek <pmiel13@gmail.com>
2024-03-02 00:34:00 +0100

    fix(browser): remove duplicate files

--- apps/browser/src/app/App.vue
+++ apps/browser/src/app/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Header from '../components/layout/Header.vue';
+import Header from './components/layout/Header.vue';
 import { ref } from 'vue';
 
 const theme = ref<'light' | 'dark'>('light');
